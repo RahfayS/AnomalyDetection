09e8cdfad100ea4b9c5da8705d3a42de9933f8be
Rahfay <saberirahfay@gmail.com>
2026-01-12 22:18:54 -0500

    Refactor load_model function to use hf_hub_download for model loading and improve caching with Streamlit

--- utils/load.py
+++ utils/load.py
@@ -1,28 +1,19 @@
 import torch
-import os
 from model import VAE
+from huggingface_hub import hf_hub_download
+import streamlit as st
 
-def load_model(pth_path = 'vae_model_utils/vae_model_best.pth', device = None):
-    """
-    Loads state dict of Variational AutoEncoder
-    
-    Args:
-        pth_path (str): Path to scratch model
-        device (torch.device): CPU or GPU. If None, auto-detect.
-    
-    Returns:
-        model (nn.Module): Ready-to-use model
-    """
+@st.cache_resource(show_spinner="Loading VAE model...")
+def load_model(device=None):
     if device is None:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    # --- Initialize Model
-    vae = VAE(channel_in=1, z = 32).to(device)
-    vae.load_state_dict(
-        torch.load(pth_path,map_location=device)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_path = hf_hub_download(
+        repo_id="Rahfay/vae-anomaly-detection",
+        filename="vae-anomaly-v1.pth"
     )
 
-    print('Model Loaded')
+    vae = VAE(channel_in=1, z=32).to(device)
+    vae.load_state_dict(torch.load(model_path, map_location=device))
     vae.eval()
-
     return vae
