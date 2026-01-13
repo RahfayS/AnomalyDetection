249d3d2564e1c1a9feff9989d580bbed613cd330
Rahfay <saberirahfay@gmail.com>
2026-01-12 19:34:02 -0500

    Refactor preprocess_image function for clarity and consistency in image transformation

--- utils/preprocess_image.py
+++ utils/preprocess_image.py
@@ -1,28 +1,29 @@
 import torch
 import torchvision.transforms as T
 from PIL import Image
-import numpy as np
+
 def preprocess_image(image):
     """
-    Docstring for preprocess_image
-    
-    :param image: Description
+    Preprocess MRI image for VAE inference
+    Output shape: (1, 1, 256, 256)
+    Output range: [-1, 1]
     """
     image = Image.open(image)
-    
+
     # --- Define Image Transform ---
     transform = T.Compose([
-        T.ToTensor(),
-        T.Lambda(lambda image: image[:3, :, :]),  # RGBA → RGB
-        T.Grayscale(num_output_channels=1),
+        T.ToTensor(),                         # [0,1]
+        T.Lambda(lambda x: x[:3, :, :]),      # RGBA → RGB
+        T.Grayscale(num_output_channels=1),   # RGB → 1 channel
         T.Resize((256, 256)),
         T.ConvertImageDtype(torch.float32),
+        T.Normalize(mean=[0.5], std=[0.5])    # [0,1] → [-1,1]
     ])
 
     # --- Transform Image ---
     image = transform(image)
-    image = image.detach().unsqueeze(1)
+
+    # --- Add batch dimension ---
+    image = image.unsqueeze(0)  # (1, 1, 256, 256)
+
     return image
-
-
-    
